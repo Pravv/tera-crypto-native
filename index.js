1782285c96dac99d731e1e3ada10cdb390d0ee26
Rafał Matysiak <Kotecek3@gmail.com>
2019-01-22 15:23:55 +0100

    fix index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const file = `${__dirname}/bin/${process.arch}/tera-crypto-native_${process.versions.modules}.node`
+const file = `${__dirname}/build/Release/tera_crypto_native.node`
 if(!fs.existsSync(file))
     throw Error(`tera-crypto-native: No build found (arch=${process.arch}, modulesVer=${process.versions.modules})`)
 
